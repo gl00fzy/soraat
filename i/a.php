e2a777bc8d5634dc78d66b2cf5a36c25ecb4ca48
gl00fzy <goffari2547@gmail.com>
2026-02-17 19:02:22 +0700

    update to cloud

--- i/a.php
+++ i/a.php
@@ -8,34 +8,46 @@
 <body>
 <h1> สวัสดี งาน i -- สรอัฐ น้ำใส (กอฟ) </h1>
 
+<form method="post">
+    ชื่อภาค <input type="text" name="rname" autofocus required>
+    <button type="submit" name="Submit">บันทึก</button>
+</form> <br><br>
+
 <?php
 include 'connectdb.php';
-
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
-
-
-while ($data = mysqli_fetch_array($rs)) {
-    echo $data['r_id']. "<br>";
-    echo $data['r_name']. "<br>";
+if (isset($_POST['Submit'])) {
+    $rname = $_POST['rname'];
+    $sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '$rname')";
+    mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
 }
-
-mysqli_close($conn);
 ?>
+
 
 <table border="1">
     <tr>
         <th>Region ID</th>
         <th>Region Name</th>
     </tr>
-    <?php
-    while ($data = mysqli_fetch_array($rs)) {
-        echo "<tr>";
-        echo "<td>" . $data['r_id'] . "</td>";
-        echo "<td>" . $data['r_name'] . "</td>";
-        echo "</tr>";
-    }
-    ?>
+
+<?php
+
+$sql = "SELECT * FROM regions";
+$rs = mysqli_query($conn, $sql);
+while ($data = mysqli_fetch_array($rs)) {
+?>
+    <tr>
+    <td><?php echo $data['r_id'] ?></td>
+    <td><?php echo $data['r_name'] ?></td>
+    <td><img src="img/delete.png" width="20" height="20"></td>
+    </tr>
+<?php
+}
+?>
+
+<?php
+mysqli_close($conn);
+?>
+
 </table>
 
 </body>
