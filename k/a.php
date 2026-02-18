f45bc80934684eff1a5d109c599a54fecbb9636d
gl00fzy <goffari2547@gmail.com>
2026-02-18 16:54:18 +0700

    file k

---
+++ k/a.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>สรอัฐ น้ำใส</title>
+</head>
+<body>
+<h1> งาน k 66010914018 สรอัฐ น้ำใส </h1> 
+<button style="background-color: green;"><a href ='2.jpg'>รูปผมเอง</a></button>
+<button style="background-color: yellow;"><a href ='1.jpg'>รูปอาจารย์</a></button>
+
+</body>
+</html>
