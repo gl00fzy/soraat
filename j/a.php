428577a6ed68f56fb0c923da9ff72df1f5350851
gl00fzy <goffari2547@gmail.com>
2026-02-18 13:27:42 +0700

    change width

--- j/a.php
+++ j/a.php
@@ -5,6 +5,7 @@
 <title>สรอัฐ น้ำใส</title>
 </head>
 <body>
-<h1> งาน j 66010914018 สรอัฐ น้ำใส </h1> <img src="img/1.jpg">
+<h1> งาน j 66010914018 สรอัฐ น้ำใส </h1> 
+<img src="img/1.jpg" width="500" height="500">
 </body>
 </html>
