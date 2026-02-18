4118aa951a037c182a3e97ce1a5d28c80fdfd388
gl00fzy <goffari2547@gmail.com>
2026-02-18 13:26:30 +0700

    add j folder

---
+++ j/a.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>สรอัฐ น้ำใส</title>
+</head>
+<body>
+<h1> งาน j 66010914018 สรอัฐ น้ำใส </h1> <img src="img/1.jpg">
+</body>
+</html>
