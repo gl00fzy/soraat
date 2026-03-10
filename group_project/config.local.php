dee25991a46b910e32dbccb3b42fbe73dacb7130
gl00fzy <goffari2547@gmail.com>
2026-03-10 15:56:20 +0700

    Remove config.local.php from tracking

--- group_project/config.local.php
+++
@@ -1,7 +0,0 @@
-<?php
-// ไฟล์นี้ใช้สำหรับตั้งค่าการเชื่อมต่อฐานข้อมูลบนเครื่อง Local (ห้ามนำขึ้น Git)
-$host = 'localhost';
-$dbname = 'shop_project';
-$username = 'root';
-$password = ''; // สำหรับเครื่อง Local ไม่ต้องใส่รหัส
-?>
